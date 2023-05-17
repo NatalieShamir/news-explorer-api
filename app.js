dd62bad432225c5bc5095f2c7f4e7167e150ff02
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-05-17 17:49:44 +0300

    protect routes using authorization

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ const app = express();
 const { userRouter } = require('./routes/users');
 const { cardRouter } = require('./routes/cards');
 const { createUser, login } = require('./controllers/users');
+const auth = require('./middlewares/auth');
 
 app.post('/signin', login);
 app.post('/signup', createUser);
-
+app.use(auth);
 app.use('/users', userRouter);
 app.use('/cards', cardRouter);
 
