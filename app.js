1f1133727db72b9dc2e58a3424232c186ffcec56
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-05-18 10:01:35 +0300

    implement logging

--- app.js
+++ app.js
@@ -8,12 +8,17 @@ const { userRouter } = require('./routes/users');
 const { cardRouter } = require('./routes/cards');
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middleware/logger');
+
+app.use(requestLogger);
 
 app.post('/signin', login);
 app.post('/signup', createUser);
 app.use(auth);
 app.use('/users', userRouter);
 app.use('/cards', cardRouter);
+
+app.use(errorLogger);
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
